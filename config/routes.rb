916251adbcf15d5113359151af8f7961e88523bb
gabalmat <gabalmat@ncsu.edu>
2019-02-15 22:24:38 +0900

    Started working on the form and index views for tours... applied minor css changes

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :customers
   resources :agents
+  resources :tours
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
