54111c3df29ea04c1a2f0fcc957b6a3714bed202
gabalmat <gabalmat@ncsu.edu>
2019-02-16 23:12:05 +0900

    Added migration to create foreign key and index for the tour_id column in the tour_locations table... still working on Tour views

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,6 +2,7 @@ class Tour < ApplicationRecord
   has_many :user_tours
   has_many :users, through: :user_tours
   has_many :tour_locations
+  accepts_nested_attributes_for :tour_locations
 
   enum status: {
       in_future: "In Future",
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_085335) do
+ActiveRecord::Schema.define(version: 2019_02_16_135623) do
 
-  create_table "tour_locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "tour_locations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "country"
     t.string "state_or_province"
     t.boolean "starting_point"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_085335) do
     t.index ["tour_id"], name: "index_tour_locations_on_tour_id"
   end
 
-  create_table "tours", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "tours", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_085335) do
     t.string "status", limit: 9
   end
 
-  create_table "user_tours", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "user_tours", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "tour_id"
     t.boolean "booked"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_085335) do
     t.index ["user_id"], name: "index_user_tours_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_085335) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "tour_locations", "tours"
   add_foreign_key "user_tours", "tours"
   add_foreign_key "user_tours", "users"
 end
