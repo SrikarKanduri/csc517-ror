b7ec06af41e8e45e3f86e30ba87697522dd54bfa
gabalmat <gabalmat@ncsu.edu>
2019-02-28 22:48:39 +0900

    changed 'tour' to '@tour' in the set_tour() function... in _form.html.erb - changed date selector elements, removed 'Add Pictures' link since it was causing an error on the create new tour page and isn't necessary on the edit tour page... in show_search.html.erb - removed booking_deadline element, changed date selector elements

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -162,7 +162,7 @@ class ToursController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tour
-      tour = Tour.find(params[:id])
+      @tour = Tour.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
