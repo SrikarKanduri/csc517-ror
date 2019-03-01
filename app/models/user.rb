d432e71029f819cee7cdd4a2fbd7d46da5a3fbb6
gabalmat <gabalmat@ncsu.edu>
2019-03-01 18:15:32 +0900

    Added password minimum length validation... Display validation errors on user sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,8 @@ class User < ApplicationRecord
   # If a user is not an admin, which is usually the case, make sure user has a first and last name
   validates :first_name, :last_name, presence: true, if: :user_is_not_admin?
 
+  validates :password, length: { minimum: 6 }
+
   # check if user is admin
   def user_is_not_admin?
     role != "admin"
